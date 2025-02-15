050b8b3b36611407ba81ba44a9d3f68f77055efa
Suhaib-git <suhaib772206881@gmail.com>
2025-02-15 22:23:16 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -22,7 +22,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' https: data:; connect-src 'self';",
+              "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' https: data:; connect-src 'self';",
           },
           {
             key: "X-Content-Type-Options",
